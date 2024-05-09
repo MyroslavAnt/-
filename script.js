41e9a30378d9c58d945694b1c9b4acd0619a269f
Myroslav Shulha <myrosllavsh@gmail.com>
2024-05-09 18:36:53 +0300

    Опис змін

--- script.js
+++ script.js
@@ -24,3 +24,95 @@
 
 
 
+// let масив = [1, 2, 3, 4, 5];
+
+// масив[0] = "нове 1";
+// масив[1] = "нове2";
+
+// console.log(масив);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = new Set([1,2,3,3,3,3])
+
+// console.log(a)
+// a.add(10)
+// a.add(40)
+
+// a.delete(2)
+
+// console.log(a.has(10))
+// console.log(a.size)
+
+// console.log(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let elementsSet = new Set();
+
+// elementsSet.add(1);
+// elementsSet.add(2);
+// elementsSet.add(3);
+
+// console.log(elementsSet);
+
+// elementsSet.clear();
+
+// console.log(elementsSet);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
