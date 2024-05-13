db14cea8aacdfcdd1b6130cefddf50ee0ec46e94
Myroslav Shulha <myrosllavsh@gmail.com>
2024-05-13 19:00:13 +0300

    Опис змін

---
+++ scriptДЗ.js
@@ -0,0 +1,64 @@
+// let mySet = new Set();
+
+// mySet.add("значення1");
+// mySet.add("значення2");
+
+// console.log(mySet);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let myMap = new Map();
+
+// myMap.set("ключ1", "значення1");
+// myMap.set("ключ2", "значення2");
+
+// console.log(myMap);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let myArray = [];
+
+// myArray.push("значення1");
+// myArray.push("значення2");
+
+// console.log(myArray);
