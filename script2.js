b0171dc1decb256d53d069c069d48ea165700652
Myroslav Shulha <myrosllavsh@gmail.com>
2024-05-24 16:52:41 +0300

    Опис змін

---
+++ script2.js
@@ -0,0 +1,113 @@
+// // Створити обєкт з полями. Привести до json виду і потім до виду обєкта. Вивести обєкт
+// let i = {
+//     name: "MrAnt",
+//     age: 13
+// }
+// let jsonString = JSON.stringify(i);
+// console.log("JSON-строка:", jsonString);
+// let newI = JSON.parse(jsonString);
+// console.log("Standart object", newI);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Створити функцію яка приймає 2 параметра строку у виді чисел. Скласти 2 параметра і потім привести до типу числа результат.
+// function Fun(rtr1, rtr2){
+//     let result = rtr1 + rtr2;
+//     rusult = Number(result)
+//     return result;
+// }
+// let string1 = "13 ";
+// let string2 = "14";
+// let sum = Fun(string1, string2);
+// console.log("Результат додавання:", sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Створити масив і знайти суму
+// let a = [983,619,5064,9,950,641]
+
+// let b = 0
+
+// for(let i = 0; i < a.length; i++){
+//     b+=a[i]
+// }
+
+// console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Створити обєкт в якому буде поле число. Створити метод який буде змінювати значення поля на число 2. Викликати через обєкт метод
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
