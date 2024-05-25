afeb92024622bba1888f2d6e35e115137f8729ad
Myroslav Shulha <myrosllavsh@gmail.com>
2024-05-25 16:00:21 +0300

    Опис змін

--- script3.js
+++ script3.js
@@ -172,3 +172,149 @@
 
 
 
+// //Створити цикл який буде виводити числа від 0 до 100. Числа які діляться на 10 без остачі пропустити. Всі інші вивести
+// //створити цикл який буде виводити числа від 20 до 50. Пропустити числа 25 та 30. Всі інші вивести
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 10 === 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// for (let i = 20; i <= 50; i++) {
+//     if (i === 25 || i === 30) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Створити обєкт  в якому буде обєкт з 2 полями. Вивести всі поля об'єктів
+// const mainObject = {
+//     innerObject: {
+//         ЯкеДніпроЯТебеЗапитую1: "НіякогоРетефула1!",
+//         ЯкеДніпроЯТебеЗапитую2: "НіякогоРетефула2!"
+//     }
+// };
+// console.log(mainObject.innerObject.ЯкеДніпроЯТебеЗапитую1);
+// console.log(mainObject.innerObject.ЯкеДніпроЯТебеЗапитую2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Створити клас в якому буде 2 поля строки. Створити клас наслідник в якому метод буде виводити найдлинішу строку
+// class ParentClass {
+//     str1 = "Φμβφυ";
+//     str2 = "Φμβφυβ";
+// }
+
+// class ChildClass extends ParentClass {
+//     printLongestString() {
+//         if (this.str1.length > this.str2.length) {
+//             console.log(this.str1);
+//         } else {
+//             console.log(this.str2);
+//         }
+//     }
+// }
+
+// const childObj = new ChildClass();
+// childObj.printLongestString();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Створити строку. Створити масив пустий та додати кожну літеру до масиву
+// const myString = "MIROSANT";
+// const myArray = [];
+// for (let i = 0; i < myString.length; i++) {
+//     myArray.push(myString[i]);
+// }
+// console.log(myArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
