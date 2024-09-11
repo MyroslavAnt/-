614f156da4ca8aecb389ac6b34ffccae734afaed
Myroslav Shulha <myrosllavsh@gmail.com>
2024-09-11 17:20:18 +0300

    Опис змін

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// let a = [1,2,3,4,5]
-// a.push(10)
-// a.unshift(10)
-// a.unshift(10)
+// let mirosant = "MirosAnt"
+// console.log(mirosant)
 
-// a.pop()
-// a.shift()
-// console.log(a)
 
 
 
@@ -24,12 +19,12 @@
 
 
 
-// let масив = [1, 2, 3, 4, 5];
 
-// масив[0] = "нове 1";
-// масив[1] = "нове2";
+// let miros = "mirosant";
+// let mirosmiros = 7;
+// let mirosmirosmiros = true;
+// console.log(typeof(miros), typeof(mirosmiros), typeof(mirosmirosmiros))
 
-// console.log(масив);
 
 
 
@@ -54,209 +49,10 @@
 
 
 
-// let a = new Set([1,2,3,3,3,3])
 
-// console.log(a)
-// a.add(10)
-// a.add(40)
 
-// a.delete(2)
 
-// console.log(a.has(10))
-// console.log(a.size)
 
-// console.log(a)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let elementsSet = new Set();
-
-// elementsSet.add(1);
-// elementsSet.add(2);
-// elementsSet.add(3);
-
-// console.log(elementsSet);
-
-// elementsSet.clear();
-
-// console.log(elementsSet);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let a = new Map([[5, 10], [40, 60]])
-
-// console.log(a.get(40))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let a = new Map([[5, 10], [40, 60]])
-
-// console.log(a.get(40))
-
-// a.set(5, 123)
-// console.log(a.get(5))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let myMap = new Map([
-//     ["ключ1', 'значення1"],
-//     ["ключ2', 'значення2"],
-//     ["ключ3', 'значення3"]
-//   ]);
-  
-//   console.log("Початковий Map");
-//   console.log(myMap);
-  
-//   myMap.delete('ключ1');
-//   myMap.delete('ключ2');
-  
-//   console.log("Map з видаленям");
-//   console.log(myMap);
-  
-//   myMap.set("ключ3", "нове значення");
-  
-//   console.log("Map після зміни значення через ключ");
-//   console.log(myMap);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function multiplyAfterDelay(a, b) {
-//     setTimeout(function() {
-//         console.log(a * b);
-//     }, 10000);
-// }
-
-// multiplyAfterDelay(5, 10);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let numbers = [];
-
-// for (let i = 100; i <= 200; i++) {
-//     numbers.push(i);
-// }
-
-// console.log(numbers);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var str = "Привіт";
-
-// var letters = str.split("");
-
-// console.log(letters);
