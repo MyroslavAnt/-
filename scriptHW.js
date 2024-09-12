7949d3f387d480dbc7e34a0a22e12521ebb9d396
Myroslav Shulha <myrosllavsh@gmail.com>
2024-09-12 15:48:34 +0300

    Опис змін

---
+++ scriptHW.js
@@ -0,0 +1,64 @@
+// let VScode = "Прпивриеивввввеееттт";
+
+// for (let i = 0; i < VScode.length; i++) {
+//   console.log(VScode[i+=2]);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function number(num1, num2) {
+//     return [String(num1), String(num2)];
+//   }
+// number()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Set(num) {
+//     return 20;
+//   }
