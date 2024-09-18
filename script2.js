4ac82d6dd282f30d5bfebf714ae9af765f0b8f11
Myroslav Shulha <myrosllavsh@gmail.com>
2024-09-18 17:27:46 +0300

    Опис змін

---
+++ script2.js
@@ -0,0 +1,72 @@
+// class Person{
+//     a = "MirosAnt"
+//     printData(){
+//         console.log("Call me")
+//     }
+// }
+
+// let unit = new Person()
+// console.log(unit.a)
+// unit.printData()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Dratytu{
+//     bbb = "MrAntPro"
+//     printData(){
+//         console.log("Call me")
+//         console.log(this.bbb)
+//     }
+// }
+
+// let unitttt = new Dratytu()
+// console.log(unitttt.bbb)
+// unitttt.printData()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
