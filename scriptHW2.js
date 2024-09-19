0c40fc834dc154fe7c9f9bce65407572ab34b056
Myroslav Shulha <myrosllavsh@gmail.com>
2024-09-19 18:35:12 +0300

    Опис змін

---
+++ scriptHW2.js
@@ -0,0 +1,110 @@
+// class RomanPro{
+//     #Roman = "MrPro"
+// }
+
+// let Green = new RomanPro()
+// let School = new RomanPro()
+// console.log(School.RomanPro)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class MrAntPro {
+//     static MrHD(str1, str2) {
+//       if (str1 < str2) {
+//         return `${str1} менше, ніж ${str2}`;
+//       } else if (str1 > str2) {
+//         return `${str1} більше, ніж ${str2}`;
+//       } else {
+//         return `${str1} і ${str2} однакові`;
+//       }
+//     }
+//   }
+  
+//   console.log(MrAntPro.MrHD("apple", "banana"));
+//   console.log(MrAntPro.MrHD("cherry", "banana"));
+//   console.log(MrAntPro.MrHD("apple", "apple"));  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class MrAntPro {
+//     constructor(str1, str2) {
+//       this.str1 = str1;
+//       this.str2 = str2;
+//     }
+  
+//     MrFullHD() {
+//       const firstChar1 = this.str1.charAt(0);
+//       const firstChar2 = this.str2.charAt(0);
+//       console.log(`Перша літера першого рядка: ${firstChar1}`);
+//       console.log(`Перша літера другого рядка: ${firstChar2}`);
+//     }
+//   }
+
+//   const example = new MrAntPro("apple", "banana");
+//   example.MrFullHD();
