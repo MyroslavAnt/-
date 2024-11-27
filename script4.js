31a4f4d2b0b3a1dcfdb5d36f3bea6e5ab5ca2921
Myroslav Shulha <myrosllavsh@gmail.com>
2024-11-27 18:29:23 +0200

    kljfd

---
+++ script4.js
@@ -0,0 +1,74 @@
+// const http = require("http");
+ 
+// const server =  http.createServer(function(request, response){
+//     response.end("Hello METANIT.COM!");
+// });
+// server.listen(3000, function(){ console.log("Сервер запущен по адресу http://localhost:3000")});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require("http");
+  
+// http.createServer(function(request, response){
+     
+//     response.setHeader("Content-Type", "text/html; charset=utf-8;");
+     
+//     if(request.url === "/home" || request.url === "/"){
+//         response.write("<h2>Home</h2>");
+//     }
+//     else if(request.url == "/about"){
+//         response.write("<h2>About</h2>");
+//     }
+//     else if(request.url == "/contact"){
+//         response.write("<h2>Contacts</h2>");
+//     }
+//     else{
+//         response.write("<h2>Not found</h2>");
+//     }
+//     response.end();
+// }).listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
