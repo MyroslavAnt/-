b288bd5def81c47d1a3964490489666dac3413d4
Myroslav Shulha <myrosllavsh@gmail.com>
2024-12-04 17:25:00 +0200

    fjk

--- script5.js
+++ script5.js
@@ -1,12 +1,64 @@
-// подключение express
-const express = require("express");
-// создаем объект приложения
-const app = express();
-// определяем обработчик для маршрута "/"
-app.get("/", function(request, response){
+// // подключение express
+// const express = require("express");
+// // создаем объект приложения
+// const app = express();
+// // определяем обработчик для маршрута "/"
+// app.get("/", function(request, response){
      
-    // отправляем ответ
-    response.send("<h2>Привет Express!</h2>");
-});
-// начинаем прослушивать подключения на 3000 порту
-app.listen(3000);
+//     // отправляем ответ
+//     response.send("<h2>Привет Express!</h2>");
+// });
+// // начинаем прослушивать подключения на 3000 порту
+// app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+ 
+// const app = express();
+// app.get("/", function(request, response){
+     
+//     response.send("<h1>Главная страница</h1>");
+// });
+// app.get("/about", function(request, response){
+     
+//     response.send("<h1>О сайте</h1>");
+// });
+// app.get("/contact", function(request, response){
+     
+//     response.send("<h1>Контакты</h1>");
+// });
+// app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
