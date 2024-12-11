76a5715ccdeb19a4c32dc8e8880a410e05f19f10
Myroslav Shulha <myrosllavsh@gmail.com>
2024-12-11 18:43:40 +0200

    jfk

--- script6.js
+++ script6.js
@@ -181,23 +181,23 @@
 
 
 
-const express = require("express");
- 
-const app = express();
- 
-app.set("view engine", "ejs");
- 
-app.use("/contact", function(request, response){
-     
-    response.render("contact", {
-        title: "Мои контакты",
-        emailsVisible: true,
-        emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
-        phone: "+1234567890"
-    });
-});
-app.use("/", function(request, response){
-     
-    response.send("Главная страница");
-});
-app.listen(3000);
+// const express = require("express");
+//  
+// const app = express();
+//  
+// app.set("view engine", "ejs");
+//  
+// app.use("/contact", function(request, response){
+//      
+//     response.render("contact", {
+//         title: "Мои контакты",
+//         emailsVisible: true,
+//         emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
+//         phone: "+1234567890"
+//     });
+// });
+// app.use("/", function(request, response){
+//      
+//     response.send("Главная страница");
+// });
+// app.listen(3000);
