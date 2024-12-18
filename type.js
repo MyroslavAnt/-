3effc2be55709e6a55b652fac66e4e6bc1457b89
Myroslav Shulha <myrosllavsh@gmail.com>
2024-12-18 18:09:14 +0200

    jfdk

--- type.js
+++ type.js
@@ -1,9 +1,28 @@
-function Fox(age, age2) {
-    if (age > age2) {
-        console.log(age2);
+"use strict";
+// function Fox(age: number, age2: number){
+//     if(age>age2){
+//         console.log(age2)
+//     }
+//     else{
+//         console.log(age)
+//     }
+// }
+// Fox(3, 7);
+Object.defineProperty(exports, "__esModule", { value: true });
+// let a: number = [2,3,4,6];
+// a[0] = 1
+// a[3] = 5
+// console.log(a)
+//Створити клас в якому 2 поля числа Створити метод який буде складати поля Через екземпляр викликати метод
+var Person = /** @class */ (function () {
+    function Person() {
+        this.name = "MirosAnt";
+        this.age = 777;
     }
-    else {
-        console.log(age);
-    }
-}
-Fox(3, 7);
+    Person.prototype.MrAntPro = function () {
+        console.log(this.name, this.age);
+    };
+    return Person;
+}());
+var a = new Person();
+a.MrAntPro();
--- type.ts
+++ type.ts
@@ -1,10 +1,60 @@
-// Створити функцію яка приймає 2 параметра числа. Функція буде виводити найменше число серед параметрів
-function Fox(age: number, age2: number){
-    if(age>age2){
-        console.log(age2)
-    }
-    else{
-        console.log(age)
+// function Fox(age: number, age2: number){
+//     if(age>age2){
+//         console.log(age2)
+//     }
+//     else{
+//         console.log(age)
+//     }
+// }
+// Fox(3, 7);
+
+import { json } from "express/lib/response"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a: number = [2,3,4,6];
+// a[0] = 1
+// a[3] = 5
+// console.log(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Створити клас в якому 2 поля числа Створити метод який буде складати поля Через екземпляр викликати метод
+class Person{
+    name: string = "MirosAnt"
+    age: number = 777
+    MrAntPro(){
+        console.log(this.name, this.age)
     }
 }
-Fox(3, 7);
+let a = new Person()
+a.MrAntPro()
