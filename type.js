221804ee3686e012b169c82dc1d479674d12bcc0
Myroslav Shulha <myrosllavsh@gmail.com>
2024-12-18 17:41:44 +0200

    fjk

--- type.js
+++ type.js
@@ -1,10 +1,10 @@
 // Створити функцію яка приймає 2 параметра числа. Функція буде виводити найменше число серед параметрів
-function Fox(age: number, age2: number){
-    if(age>age2){
-        console.log(age2)
+function Fox(age, age2) {
+    if (age > age2) {
+        console.log(age2);
     }
-    else{
-        console.log(age)
+    else {
+        console.log(age);
     }
 }
 Fox(3, 7);
